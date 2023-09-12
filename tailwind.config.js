b5f2a4d06f44ec684d886554ef941dad8f5e298d
Krishandeep Singh <krishandeep17@gmail.com>
2023-09-12 12:19:03 +0530

    add prettier-plugin-tailwindcss and update tailwind css version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx}"],
-  mode: "jit",
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
